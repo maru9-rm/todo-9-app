32dfc3c814ea1aaa7d6af2b5ae1e4c863825e95b
Ryota9 <ngm9dwf@gmail.com>
2021-04-03 14:45:19 +0900

    Boardモデルの作成とboardコントローラーとかindexとかその辺りの作成

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,16 @@
+class BoardsController < ApplicationController
+    def index
+        @title = 'デイトラ'
+    end
+
+    def new
+        @title = 'デイトラ'
+    end
+
+
+    def edit
+        @title = 'デイトラ'
+    end
+
+
+end
