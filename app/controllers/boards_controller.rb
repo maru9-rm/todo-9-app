837641bfa396ce25b043f238f53726b3f625c2eb
Ryota9 <ngm9dwf@gmail.com>
2021-04-05 17:40:39 +0900

    ユーザとボードを紐付けた！

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,5 @@
 class BoardsController < ApplicationController
-    before_action :set_board, only: [:show, :edit, :update]
+    before_action :set_board, only: [:show]
     before_action :authenticate_user!
 
 
@@ -12,11 +12,11 @@ class BoardsController < ApplicationController
 
 
     def new
-        @board = Board.new
+        @board = current_user.boards.build
     end
 
     def create
-        @board = Board.new(board_params)
+        @board = current_user.boards.build(board_params)
         if @board.save
             redirect_to board_path(@board), notice: 'Successfully saved'
         else
@@ -27,9 +27,11 @@ class BoardsController < ApplicationController
 
 
     def edit
+        @board = current_user.boards.find(params[:id])
     end
 
     def update
+        @board = current_user.boards.find(params[:id])
         if @board.update(board_params)
             redirect_to board_path(@board), notice: 'Successfully changed'
         else
@@ -39,7 +41,7 @@ class BoardsController < ApplicationController
     end
 
     def destroy
-        board = Board.find(params[:id])
+        board = current_user.boards.find(params[:id])
         board.destroy!
         redirect_to root_path, notice: 'Successfully deleted'
     end
