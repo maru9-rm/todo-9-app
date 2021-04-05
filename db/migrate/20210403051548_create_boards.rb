fa1c8489fd211729a8249fa35f3460f6e490f9bc
Ryota9 <ngm9dwf@gmail.com>
2021-04-06 00:03:39 +0900

    マイグレーションファイルの作り直し。ついでにフォームボタンのcssおかしかったので修正。

--- db/migrate/20210403051548_create_boards.rb
+++ db/migrate/20210403051548_create_boards.rb
@@ -3,6 +3,7 @@ class CreateBoards < ActiveRecord::Migration[6.0]
     create_table :boards do |t|
       t.string :title
       t.text :content
+      t.references :user , null: false 
       t.timestamps
     end
   end
